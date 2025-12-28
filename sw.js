e4faa81da2b2863bba89eff0b169f2377290d321
baschub <schubert.bastian@googlemail.com>
2025-12-28 19:42:26 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'meine-pwa-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+// Dateien in den Cache laden
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Anfragen über den Cache beantworten
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
